6676dc77b1b736441c82af2ee388e368fb53a90f
Joshua Paino <joshua.paino@gmail.com>
2023-01-20 19:15:35 -0600

    Datatypes

---
+++ 1.IntroductionToJava/untitled/src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        byte numCredits  = 3;
+        char myGrade = 'A';
+        short numCourses = 4;
+        int age = 19;
+        long tuitionPaid = 23983L;
+        float gpa = 3.21f;
+        double incomeEarned = 8283.98d;
+
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("Byte: " + numCredits);
+        System.out.println("Char: " + myGrade);
+        System.out.println("Short: " + numCourses);
+        System.out.println("Int: " + age);
+        System.out.println("Long: " + tuitionPaid);
+        System.out.println("Float: " + gpa);
+        System.out.println("Double: " + incomeEarned);
+
+    }
+}
--- 1.IntroductionToJava/untitled/src/MyFirstProgram.java
+++ 1.IntroductionToJava/untitled/src/MyFirstProgram.java
@@ -1,10 +1,25 @@
+
 public class MyFirstProgram {
+
+    public static int staticVar = 20;
     public static void main(String[] args){
-        /*
-        Multi Line Comment
-        */
-        // My First Comment
-        System.out.println("My First Java Program"); //In-line Comment
+        final int maxRating = 10;
+        int varInt;
+
+        varInt = 50;
+
+        System.out.println(staticVar);
+
+        System.out.format("The integer %d is the value of the variable.", varInt );
+        String varString = "This is a string\n";
+
+        System.out.println(varString);
+        System.out.format(varString);
+        System.out.format("The string value: " + varString);
+        int number = 20;
+        System.out.println(number); //In-line Comment
+        System.out.println(varInt); //In-line Comment
+        System.out.println(maxRating); //In-line Comment
     }
 
 
