c64ae1eb99cb02ae6ef82f52280003e61f58b78c
Joshua Paino <joshua.paino@gmail.com>
2023-01-22 19:40:36 -0600

    Arithmetic Operators

---
+++ 1.IntroductionToJava/untitled/src/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        int num1 = 20, num2 = 10, result;
+        result = num1 + num2;
+        System.out.println("num1 + num2 = " + result);
+
+        double numA = 20D, numB = 10D, res;
+        res = numA - numB;
+        System.out.println("numA - numB = " + res);
+
+        float numFA = 10F, numFB = 10F, resF;
+        resF = numFA * numFB;
+        System.out.println("numFA * numFB = " + resF);
+
+
+    }
+}
