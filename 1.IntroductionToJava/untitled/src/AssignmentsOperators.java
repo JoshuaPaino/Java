53d0f950408a554cfbc11768b7730341c6c93419
Joshua Paino <joshua.paino@gmail.com>
2023-01-22 19:53:56 -0600

    Assignment Operators

---
+++ 1.IntroductionToJava/untitled/src/AssignmentsOperators.java
@@ -0,0 +1,18 @@
+public class AssignmentsOperators {
+    public static void main(String[] args) {
+        int varInt1 = 10, varInt2 = 5,varInt3 = 5;
+        System.out.println("varInt1 = " + varInt1);
+        System.out.println("varInt2 = " + varInt2);
+        System.out.println("varInt3 = " + varInt3);
+        varInt1 += 1;
+        varInt2++;
+        varInt3--;
+        System.out.println("varInt1 after an increment" + varInt1);
+        System.out.println("varInt2 after an increment " + varInt2);
+        System.out.println("varInt3 after an increment " + varInt3);
+
+        
+
+    }
+}
+
